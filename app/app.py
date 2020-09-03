1d07c1a7705b267d93b4f7cd99420794ff63a5a2
Brian Schaefer <physinet@gmail.com>
2020-09-03 13:30:42 -0400

    Example API call and display data

--- app/app.py
+++ app/app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
+import requests
 
 app = Flask(__name__)
 
+API_KEY = 'JZ83OOJHHC5428Z3'
+symbol='IBM'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY\
+&outputsize=full&symbol={}&apikey={}'.format(symbol, API_KEY)
 
-data = {'test': [1,2,3], 'testagain': [3,4,5]}
+r = requests.get(url)
+
+df = pd.DataFrame(r.json()['Time Series (Daily)'], dtype=float)
+df = df.T # transpose so dates are in the rows
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=data)
+  return render_template('index.html', data=df)
 
 if __name__ == '__main__':
   app.run(port=33507)
