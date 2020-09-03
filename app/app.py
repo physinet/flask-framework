930ba020af760597f812af671f59a76cfe1b1233
Brian Schaefer <physinet@gmail.com>
2020-09-03 13:23:35 -0400

    Some initial edits
    Try to show a dictionary

--- app/app.py
+++ app/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
+data = {'test': [1,2,3], 'testagain': [3,4,5]}
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', data=data)
 
 if __name__ == '__main__':
   app.run(port=33507)
